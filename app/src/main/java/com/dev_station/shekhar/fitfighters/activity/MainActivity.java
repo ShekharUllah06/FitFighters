6723f45d29f3718d13b8a98cb16f0c2299ba7294
Shekhar Ullah <ullah.shekhar@gmail.com>
2017-03-11 09:17:26 +0600

    fragments added

--- app/src/main/java/com/dev_station/shekhar/fitfighters/activity/MainActivity.java
+++ app/src/main/java/com/dev_station/shekhar/fitfighters/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.dev_station.shekhar.fitfighters;
+package com.dev_station.shekhar.fitfighters.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.dev_station.shekhar.fitfighters.R;
 
 public class MainActivity extends AppCompatActivity {
 
